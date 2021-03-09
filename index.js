c622e17aa8221ce4a283465dfbeb5362e81c4b85
Jblack017 <blackhamjoshua@comcast.net>
2021-03-08 22:14:30 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+//Thank you card creator
+
+//
+const name = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const cards = []
+
+function writeCards(name, var2 = "surprise") {
+  for (let i = 0; i < name.length; i++) {
+    
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${var2} gift!`);
+    
+    }
+    
+    return cards;
+}
+
+writeCards(cards); 
+
+
+function countDown(var1) {
+
+    let countUp = var1;
+while (countUp >= 0) {
+  console.log(countUp--);
+
+    } 
+return var1
+}
